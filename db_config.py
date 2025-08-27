03387c47091366d548fee3415fc44d9e4ccae69d
Adam shaker <adammohamedshaker3@gmail.com>
2025-08-27 20:42:17 +0300

    Add files via upload

--- db_config.py
+++ db_config.py
@@ -1,8 +1,13 @@
 import mysql.connector
+from mysql.connector import pooling
+connection_pool = pooling.MySQLConnectionPool(
+    pool_name="mypool",
+    pool_size=32, 
+    pool_reset_session=True,
+    host='localhost',
+    user='root',
+    password='HelloItsMeAdam$12',
+    database='website_driving_academy'
+)
 def get_connection():
-    return mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='HelloItsMeAdam$12',
-        database='website_driving_academy'
-    )
+    return connection_pool.get_connection()
